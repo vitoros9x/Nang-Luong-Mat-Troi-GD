f5796b779b0600b621854a2eed450fcf051a3f0a
Tran Tu <68361206+vitoros9x@users.noreply.github.com>
2021-01-05 14:25:54 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$('.cat-product__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  focusOnSelect: true,
+  centerMode: true
+});
+
+// $('.slick-next').html('<i class="fa fa-chevron-right" aria-hidden="true"></i>');
