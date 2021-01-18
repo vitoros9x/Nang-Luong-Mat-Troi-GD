7b1fadf71cdace2910654b96f0f51e622422fcfb
Tran Tu <68361206+vitoros9x@users.noreply.github.com>
2021-01-18 10:08:45 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,19 @@ $('.cat-product__slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   focusOnSelect: true,
+  centerPadding: 0,
+  centerMode:true
 });
 
-// $('.slick-next').html('<i class="fa fa-chevron-right" aria-hidden="true"></i>');
+//loop: true,
+// autoplay: true,
+// autoplayTimeout: 2000,
+// autoplayHoverPause: true,
+// dots: false,
+// infinite: true,
+// centerPadding: 0,
+// slidesToShow: 3,
+// slidesToScroll: 3,
+// arrows: true,
+// centerMode: true,
+// focusOnSelect: true,
